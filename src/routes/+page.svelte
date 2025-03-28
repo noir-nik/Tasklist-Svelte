1c990a0cff9768b7bf213eba9a21ee8fef74dd14
nm <niki8fw@gmail.com>
2025-03-28 20:46:29 +0300

    Add tw3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline text-pink-600">Hello world!</h1>
+<a class="text-mind-600 hover:underline" href="/about">About</a>
---
+++ tailwind.config.ts
@@ -0,0 +1,1 @@
+F:/Temp/tailwind.config.ts
